9f8eea10ee2924228a94a074478cde78b4bdb2cd
failproblem <failproblem@gmail.com>
2023-05-22 17:00:03 +0300

    Добавил логику вычисления и вывода в консоль суммы, разности, произведения и частного двух чисел

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите число №1:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите число №2:");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сумма:" + sum);
+        System.out.println("Разность:" + difference);
+        System.out.println("Произведение:" + multiplication);
+        System.out.println("Частное:" + quotient);
     }
 }
