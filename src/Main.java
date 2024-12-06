a6c930104d2f888c4a4f25a17661c4c5538951df
tryfailsme <tryfailsme@gmail.com>
2024-12-06 23:47:22 +0300

    Добавлен бесконечный цикл в рамках "Курсовой работы: Циклы", проверяющий путь к файлу.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -5,5 +6,21 @@ public class Main {
         System.out.println("Введите текст и нажмите <Enter>:");
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста: " + text.length());
+
+        int n = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists || isDirectory) {
+                System.out.println("Файл не существует или указанный путь является путём к папке, а не к файлу.");
+                continue;
+            } else {
+                n++;
+                System.out.println("Путь указан верно.");
+                System.out.println("Это файл номер: " + n);
+            }
+        }
     }
 }
